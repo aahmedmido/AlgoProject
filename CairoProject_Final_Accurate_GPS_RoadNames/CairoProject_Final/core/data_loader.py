6957422d17362852c88b5aa66da4170061dbd6d9
Ahmed Ibrahim <ahmedibrahima714@gmail.com>
2026-05-11 20:22:46 +0300

    Fix JSON filenames for Streamlit Cloud

--- CairoProject_Final_Accurate_GPS_RoadNames/CairoProject_Final/core/data_loader.py
+++ CairoProject_Final_Accurate_GPS_RoadNames/CairoProject_Final/core/data_loader.py
@@ -22,23 +22,29 @@ class CairoTransportationData:
         self.graph = nx.Graph()
 
     def _read_json(self, filename: str):
-
         path = self.data_dir / filename
-
         with path.open("r", encoding="utf-8") as file:
             return json.load(file)
 
     def load(self):
-
         self.neighborhoods = self._read_json("neighborhoods.json")
         self.facilities = self._read_json("facilities.json")
-        self.existing_roads = self._read_json("existing_roads.json")
-        self.new_roads = self._read_json("new_roads.json")
-        self.traffic_patterns = self._read_json("traffic_patterns.json")
-        self.metro_lines = self._read_json("metro_lines.json")
-        self.bus_routes = self._read_json("bus_routes.json")
-        self.public_transport_demand = self._read_json(
-            "public_transport_demand.json"
-        )
+
+        roads_data = self._read_json("roads.json")
+        transport_data = self._read_json("transport.json")
+
+        if isinstance(roads_data, dict):
+            self.existing_roads = roads_data.get("existing_roads", roads_data.get("roads", []))
+            self.new_roads = roads_data.get("new_roads", roads_data.get("potential_roads", []))
+            self.traffic_patterns = roads_data.get("traffic_patterns", [])
+        else:
+            self.existing_roads = roads_data
+
+        if isinstance(transport_data, dict):
+            self.metro_lines = transport_data.get("metro_lines", [])
+            self.bus_routes = transport_data.get("bus_routes", [])
+            self.public_transport_demand = transport_data.get("public_transport_demand", [])
+        else:
+            self.bus_routes = transport_data
 
         return self
